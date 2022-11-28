4fe374e3023e9b245446eee6efc3c7debb8a32a4
Borhan uddin Amin <Borhanuddinamin@gmail.com>
2022-11-28 22:33:34 +0600

    exam 2 lack foe electricity

---
+++ Exam2/Task2/Form.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public partial class Form
+    {
+        int _value1;
+        string _value2;
+        double _value3;
+        bool _value4;
+        DateTime _value5;
+
+        
+    }
+}
---
+++ Exam2/Task4/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public  class Product: Iproduct
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+        public   void GenerateQRCode()
+        {
+
+        }
+        public  void GetDiscount(double discountPercent)
+        {
+
+        }
+
+        public string? GetShortName()
+        {
+            return Name?.Substring(0, 5);
+        }
+    }
+}
