5888ca80f24b1820ade53959d518e174afdc7f51
Borhan uddin Amin <Borhanuddinamin@gmail.com>
2022-12-31 11:48:50 +0600

    commit

---
+++ Assignment_2/144_A. Arrival of the General/Program.cs
@@ -0,0 +1,148 @@
+﻿// See https://aka.ms/new-console-template for more information
+int line = Convert.ToInt32(Console.ReadLine());
+
+int[] intvalue = new int[line];
+
+    string str = Console.ReadLine();
+intvalue = Array.ConvertAll(str.Split(' '), int.Parse);
+int max_index= 0;
+int min_index = 0;
+int max_value = intvalue[0];
+int min_value = intvalue[0];
+for (int i = 0; i < intvalue.Length; i++)
+{
+       if(min_value >= intvalue[i ])
+        {
+       
+            min_value =  intvalue[i];
+    
+        min_index = i + 1;
+        }
+    
+    if (max_value <= intvalue[i])
+        {
+            max_value =   intvalue[i];
+        
+        max_index = i + 1;
+        }
+        
+
+}
+// max_index= + 1;
+//min_index =+ 1;
+
+//Console.WriteLine(min_index);
+//Console.WriteLine(max_index);
+//Console.WriteLine(middle);
+
+double middle = line / 2;
+
+
+
+int max_Ds = 0;
+int min_Ds = 0;
+
+if (min_index < line)
+{
+     
+     min_Ds = line - (min_index) ;
+
+}
+if(max_index > 1)
+{
+    max_Ds = max_index-1 ;
+}
+
+//Console.WriteLine(max_Ds);
+//Console.WriteLine(min_Ds);
+
+
+
+if ((min_index == line) && (max_index == 1))
+{
+    Console.WriteLine("1");
+
+}else if ((min_Ds == 1) && (max_Ds == 1))
+{
+    Console.WriteLine("1");
+
+
+}
+int result = max_Ds + min_Ds;
+
+if ((max_Ds > middle) && (min_Ds > middle))
+{
+    result = result - 1;
+
+    Console.WriteLine(result);
+    //Console.WriteLine(max_Ds);
+}
+else if((max_Ds < middle) && (min_Ds < middle)&& (max_Ds > 1) && (min_Ds > 1))
+{
+
+    Console.WriteLine(result);
+}
+/*else if (min_Ds > middle)
+{
+    result = result - 1;
+    //Console.WriteLine(min_Ds);
+}*/
+//Console.WriteLine(max_Ds);
+//Console.WriteLine(min_Ds);
+
+
+
+
+
+
+
+
+
+
+
+/*if (max_Ds > min_Ds)
+{
+    //int result = max_index - min_index - 1;
+
+    Console.WriteLine(max_Ds);
+}
+else if (max_Ds < min_Ds)
+{
+   // int result = min_index - max_index - 1;
+    Console.WriteLine(min_Ds);
+}
+else if (max_Ds == min_Ds)
+{
+    int result = max_Ds + min_Ds;
+    Console.WriteLine(result);
+
+}
+*/
+
+
+
+
+
+
+
+
+/*
+if (max_index > min_index && min_index != 0)
+{
+    int result =  max_index- min_index  - 1;
+
+    Console.WriteLine(result);
+}
+else if (max_index < min_index && min_index != 0)
+{
+    int result =  min_index- max_index  -1 ;
+    Console.WriteLine(result);
+}
+else if(max_index < min_index && min_index == 0)
+{
+    int result = (min_index - max_index - 1) * 2;
+    Console.WriteLine(result);
+
+}
+
+*/
---
+++ Assignment_2/443_A.Anton and Letters/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Runtime.Intrinsics.X86;
+StringCollection remove = new StringCollection();
+
+
+string st = Console.ReadLine();
+string[] values= st.Split(',',' ', '{', '}').ToArray();
+string str2 = string.Join("", values);
+
+
+string  [] ch = new string [str2.Length];
+int i = 0;
+foreach(var s in str2)
+{
+    ch[i]=Convert.ToString(s);
+    i++;
+}
+
+
+int l = 0;
+int m = 0;
+
+
+remove.AddRange(ch);
+
+for (int b = 0; b < str2.Length; b++)
+{
+
+
+    for (int a =b+ 1; a< str2.Length; a++)
+    {
+        
+
+        if (ch[b] == ch[a])
+        {
+            //remove.RemoveAt(a - l)
+         Console.WriteLine( a );
+            Console.WriteLine(a - l);
+            l++;
+         
+       //ch = ch.Where(e => e == ch[a]).ToArray();
+
+        }
+        /*else if (ch[b] != ch[a])
+        {
+            m=m+1;
+            //ch = ch.Where(e => e == ch[a]).ToArray();
+            Console.WriteLine(m);
+        }*/
+    }
+    if (l >0)
+    {
+
+
+
+       
+    }
+   
+
+
+}
+
+foreach (var c in remove)
+{
+    Console.WriteLine(c);
+}
+
+
+Console.WriteLine(l);
+
+//Console.WriteLine(str2.Length );
+
+//Console.WriteLine(l);
+//Console.WriteLine(m);
+
+//Console.WriteLine(m);
+//Console.WriteLine(sum);
+/*for (int m=0; m< str2.Length;m++)
+    {
+        if (value2 == ch[m])
+        {
+            sum++;
+            Console.WriteLine(sum);
+        }
+        
+    }
+*/
